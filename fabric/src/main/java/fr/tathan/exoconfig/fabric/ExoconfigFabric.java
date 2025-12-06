1005e1d69aaf8c9fd278a19b23c9dc04d2da5eda
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-12-06 16:27:25 -0500

    fix packet registry

--- fabric/src/main/java/fr/tathan/exoconfig/fabric/ExoconfigFabric.java
+++ fabric/src/main/java/fr/tathan/exoconfig/fabric/ExoconfigFabric.java
@@ -1,12 +1,14 @@
 package fr.tathan.exoconfig.fabric;
 
 import fr.tathan.exoconfig.ExoConfig;
+import fr.tathan.exoconfig.fabric.network.NetworkRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public final class ExoconfigFabric implements ModInitializer {
     @Override
     public void onInitialize() {
+        NetworkRegistry.init();
         ExoConfig.init();
         ServerLifecycleEvents.SYNC_DATA_PACK_CONTENTS.register(ExoConfig::syncConfigs);
 
