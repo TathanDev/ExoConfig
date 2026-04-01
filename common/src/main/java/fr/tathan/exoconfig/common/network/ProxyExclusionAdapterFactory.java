40f64a395baf934653ab194777600a459de87bf4
st0x0ef <gabriel.gaudreault3@gmail.com>
2026-04-01 11:39:35 -0400

    small cleanup

--- common/src/main/java/fr/tathan/exoconfig/common/network/ProxyExclusionAdapterFactory.java
+++ common/src/main/java/fr/tathan/exoconfig/common/network/ProxyExclusionAdapterFactory.java
@@ -16,15 +16,14 @@ public class ProxyExclusionAdapterFactory implements TypeAdapterFactory {
     @Override
     public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
         if (Proxy.class.isAssignableFrom(type.getRawType())) {
-            return (TypeAdapter<T>) new TypeAdapter<>() {
+            return new TypeAdapter<>() {
                 @Override
                 public void write(JsonWriter out, Object value) throws IOException {
                     out.nullValue();
-
                 }
 
                 @Override
-                public Object read(JsonReader in) throws IOException {
+                public T read(JsonReader in) throws IOException {
                     in.skipValue();
                     return null;
                 }
